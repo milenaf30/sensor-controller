558e885cb7665332c368b2c9b12b7bc7cd0b9100
milenafarotto <milena@fetcher.ai>
2021-08-03 23:09:35 +0200

    Spring Initializer
    Project based on Spring Initializer for RestAPIs

---
+++ src/main/java/com/milena/sensorcontroller/SensorControllerApplication.java
@@ -0,0 +1,13 @@
+package com.milena.sensorcontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SensorControllerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/milena/sensorcontroller/SensorControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.milena.sensorcontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
