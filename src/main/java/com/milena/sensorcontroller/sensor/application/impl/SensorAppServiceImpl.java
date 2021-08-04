8f3377fbc72c70d73fcb67f15f54b00f23c36510
milenafarotto <milena@fetcher.ai>
2021-08-04 17:00:26 +0200

    Add logger

--- src/main/java/com/milena/sensorcontroller/sensor/application/impl/SensorAppServiceImpl.java
+++ src/main/java/com/milena/sensorcontroller/sensor/application/impl/SensorAppServiceImpl.java
@@ -4,11 +4,14 @@ import com.milena.sensorcontroller.sensor.application.SensorAppService;
 import com.milena.sensorcontroller.sensor.domain.Sensor;
 import com.milena.sensorcontroller.sensor.dto.SensorDto;
 import com.milena.sensorcontroller.sensor.service.SensorService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SensorAppServiceImpl implements SensorAppService {
 
+    Logger logger = LoggerFactory.getLogger(SensorAppServiceImpl.class);
     private final SensorService sensorService;
 
     public SensorAppServiceImpl(SensorService sensorService) {
@@ -17,9 +20,18 @@ public class SensorAppServiceImpl implements SensorAppService {
 
     @Override
     public SensorDto getByUUID(String uuid) {
-        Sensor sensor = sensorService.findByUUID(uuid);
-        return SensorDto.builder()
-                .status(Sensor.SensorStatus.OK)
-                .build();
+        logger.info(String.format("Retrieving sensor's status by uuid %s", uuid));
+        try {
+            Sensor sensor = sensorService.findByUUID(uuid);
+            SensorDto sensorDto = SensorDto.builder()
+                    .status(sensor.getStatus())
+                    .build();
+            logger.info(String.format("Sensor's status by uuid %s retrieved", uuid));
+            return sensorDto;
+        } catch (Exception ex) {
+            logger.error(String.format("Error retrieving sensor's status by uuid %s", uuid));
+            logger.error(ex.getMessage());
+            throw ex;
+        }
     }
 }
