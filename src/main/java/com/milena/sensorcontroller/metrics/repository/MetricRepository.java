3024d5cdde76e11599da3a9893cab0dc49f11da8
milenafarotto <milena@fetcher.ai>
2021-08-06 08:45:31 +0200

    Create queries to get last 30 day records
    Needed to calculate max and avg values by sensor's id.

--- src/main/java/com/milena/sensorcontroller/metrics/repository/MetricRepository.java
+++ src/main/java/com/milena/sensorcontroller/metrics/repository/MetricRepository.java
@@ -1,7 +1,9 @@
 package com.milena.sensorcontroller.metrics.repository;
 
 import com.milena.sensorcontroller.metrics.domain.Metric;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Date;
@@ -9,5 +11,23 @@ import java.util.Date;
 @Repository
 public interface MetricRepository extends PagingAndSortingRepository<Metric, Integer> {
     Metric findByDateAndSensorId(Date date, Integer sensorId);
+
+    @Query(nativeQuery = true,
+            value = "SELECT MAX(max) FROM  metrics_per_day \n " +
+                    "WHERE date BETWEEN CURDATE() - INTERVAL 30 DAY AND CURDATE() \n " +
+                    "and sensor_id = (:sensorId)")
+    Integer findMaxLast30DaysFromSensorId(@Param("sensorId") Integer sensorId);
+
+    @Query(nativeQuery = true,
+            value = "SELECT sum(sum) FROM  metrics_per_day \n " +
+                    "WHERE date BETWEEN CURDATE() - INTERVAL 30 DAY AND CURDATE() \n " +
+                    "and sensor_id = (:sensorId)")
+    Integer findSumLast30DaysFromSensorId(@Param("sensorId") Integer sensorId);
+
+    @Query(nativeQuery = true,
+            value = "SELECT sum(total_records) FROM  metrics_per_day \n " +
+                    "WHERE date BETWEEN CURDATE() - INTERVAL 30 DAY AND CURDATE() \n " +
+                    "and sensor_id = (:sensorId)")
+    Integer findTotalRecordsLast30DaysFromSensorId(@Param("sensorId") Integer sensorId);
 }
 
--- src/test/java/com/milena/sensorcontroller/metric/repository/MetricRepositoryTest.java
+++ src/test/java/com/milena/sensorcontroller/metric/repository/MetricRepositoryTest.java
@@ -1,6 +1,8 @@
 package com.milena.sensorcontroller.metric.repository;
 
+import com.milena.sensorcontroller.common.date.DateFactory;
 import com.milena.sensorcontroller.common.uuid.UUIDFactory;
+import com.milena.sensorcontroller.measurement.domain.Measurement;
 import com.milena.sensorcontroller.metrics.domain.Metric;
 import com.milena.sensorcontroller.metrics.repository.MetricRepository;
 import com.milena.sensorcontroller.sensor.domain.Sensor;
@@ -39,11 +41,119 @@ public class MetricRepositoryTest {
                 .max(200)
                 .sensorId(sensorRetrieved.getId())
                 .totalRecords(1)
-                .sum(10)
+                .sum(200)
                 .date(new Date())
                 .build();
         Metric metricRetrieved = metricRepository.save(metric);
+        Assert.assertNotNull(metric.getId());
         Assert.assertEquals(metric.getMax(), metricRetrieved.getMax());
         Assert.assertEquals(metric.getDate(), metricRetrieved.getDate());
+        Assert.assertEquals(metric.getSensorId(), sensorRetrieved.getId());
+        Assert.assertEquals(metric.getSum(), metricRetrieved.getSum());
+    }
+
+    @Test
+    public void When_FindMaxLast30Days_ThenCorrect() {
+        Sensor sensor = Sensor.builder().uuid(UUIDFactory.create()).build();
+        Date today = DateFactory.now();
+        Date yesterday = DateFactory.yesterday();
+        Integer carbonDioxideLevel = 2000;
+        Integer maxCarbonDioxideLevel = 4000;
+        Sensor sensorRetrieved = testEntityManager.persist(sensor);
+
+        Measurement measurement = Measurement.builder()
+                .time(today)
+                .carbonDioxideLevel(carbonDioxideLevel)
+                .sensor(sensorRetrieved)
+                .build();
+        Measurement measurement2 = Measurement.builder()
+                .time(yesterday)
+                .carbonDioxideLevel(maxCarbonDioxideLevel)
+                .sensor(sensorRetrieved)
+                .build();
+
+        Metric metric = Metric.builder()
+                .measurement(measurement)
+                .build();
+        metricRepository.save(metric);
+
+        Metric metric2 = Metric.builder()
+                .measurement(measurement2)
+                .build();
+        metricRepository.save(metric2);
+
+        Integer max = metricRepository.findMaxLast30DaysFromSensorId(sensor.getId());
+
+        Assert.assertEquals(maxCarbonDioxideLevel, max);
+    }
+
+    @Test
+    public void When_FindSumLast30Days_ThenCorrect() {
+        Sensor sensor = Sensor.builder().uuid(UUIDFactory.create()).build();
+        Date today = DateFactory.now();
+        Date yesterday = DateFactory.yesterday();
+        Integer carbonDioxideLevel = 2000;
+        Integer maxCarbonDioxideLevel = 4000;
+        Sensor sensorRetrieved = testEntityManager.persist(sensor);
+
+        Measurement measurement = Measurement.builder()
+                .time(today)
+                .carbonDioxideLevel(carbonDioxideLevel)
+                .sensor(sensorRetrieved)
+                .build();
+        Measurement measurement2 = Measurement.builder()
+                .time(yesterday)
+                .carbonDioxideLevel(maxCarbonDioxideLevel)
+                .sensor(sensorRetrieved)
+                .build();
+
+        Metric metric = Metric.builder()
+                .measurement(measurement)
+                .build();
+        metricRepository.save(metric);
+
+        Metric metric2 = Metric.builder()
+                .measurement(measurement2)
+                .build();
+        metricRepository.save(metric2);
+
+        Integer sum = metricRepository.findSumLast30DaysFromSensorId(sensor.getId());
+
+        Assert.assertEquals(Integer.valueOf(maxCarbonDioxideLevel + carbonDioxideLevel), sum);
+    }
+
+    @Test
+    public void When_FindTotalRecordsLast30Days_ThenCorrect() {
+        Sensor sensor = Sensor.builder().uuid(UUIDFactory.create()).build();
+        Date today = DateFactory.now();
+        Date yesterday = DateFactory.yesterday();
+        Integer carbonDioxideLevel = 2000;
+        Integer maxCarbonDioxideLevel = 4000;
+        Sensor sensorRetrieved = testEntityManager.persist(sensor);
+
+        Measurement measurement = Measurement.builder()
+                .time(today)
+                .carbonDioxideLevel(carbonDioxideLevel)
+                .sensor(sensorRetrieved)
+                .build();
+        Measurement measurement2 = Measurement.builder()
+                .time(yesterday)
+                .carbonDioxideLevel(maxCarbonDioxideLevel)
+                .sensor(sensorRetrieved)
+                .build();
+
+        Metric metric = Metric.builder()
+                .measurement(measurement)
+                .build();
+        metricRepository.save(metric);
+
+        Metric metric2 = Metric.builder()
+                .measurement(measurement2)
+                .build();
+        metricRepository.save(metric2);
+
+        Integer totalRecords = metricRepository.findTotalRecordsLast30DaysFromSensorId(sensor.getId());
+
+        Assert.assertEquals(Integer.valueOf(2), totalRecords);
     }
 }
