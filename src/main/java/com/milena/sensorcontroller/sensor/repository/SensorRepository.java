9d28ad3fb31f830e5b6c1deb9f6b952b000b8c1f
milenafarotto <milena@fetcher.ai>
2021-08-06 09:37:19 +0200

    Add Repository tag to SensorRepository

--- src/main/java/com/milena/sensorcontroller/sensor/repository/SensorRepository.java
+++ src/main/java/com/milena/sensorcontroller/sensor/repository/SensorRepository.java
@@ -2,7 +2,9 @@ package com.milena.sensorcontroller.sensor.repository;
 
 import com.milena.sensorcontroller.sensor.domain.Sensor;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SensorRepository extends PagingAndSortingRepository<Sensor, Long> {
     Sensor findByUuid(String uuid);
 }
